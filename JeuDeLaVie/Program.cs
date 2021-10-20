3690bb3762c62297820b84df64dc55f89e9a3980
coco3167 <corent1.creully@sfr.fr>
2021-10-20 11:17:05 +0200

    Ajout struct Coords et class Cell

--- JeuDeLaVie/Program.cs
+++ JeuDeLaVie/Program.cs
@@ -2,6 +2,48 @@
 
 namespace JeuDeLaVie
 {
+    struct Coords
+    {
+        public readonly int x;
+        public readonly int y;
+        public Coords(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+
+    public class Cell
+    {
+        private bool isAlive;
+        private bool nextState;
+
+        public Cell(bool state)
+        {
+            isAlive = state;
+        }
+
+        public void ComeAlive()
+        {
+            nextState = true;
+        }
+
+        public void PassAway()
+        {
+            nextState = false;
+        }
+
+        public void Update()
+        {
+            isAlive = nextState;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
